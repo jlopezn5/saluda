003b6e7226bff149da46b2796ef4cba21fd61305
jose <jlopezn5@gmail.com>
2020-03-11 14:03:09 +0100

    Añado edad

--- src/saluda/Saluda.java
+++ src/saluda/Saluda.java
@@ -19,10 +19,14 @@ public class Saluda {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nombre;
+        int edad;
         System.out.println("Introduzca su nombre:");
         nombre = sc.nextLine();
+        System.out.println("Introduzca la edad:");
+        edad = sc.nextInt();
+        System.out.println("Hola "+ nombre + " tienes " + edad +" años");
         
-        System.out.println("Hola "+ nombre);
+        
     }
     
 }
